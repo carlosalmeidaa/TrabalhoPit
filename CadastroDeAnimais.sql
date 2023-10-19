ff81f2e6972108e0f2b1cee0b361b01fb8710fce
carlosalmeidaa <111435199+carlosalmeidaa@users.noreply.github.com>
2023-10-19 18:23:34 -0300

    Add files via upload

---
+++ CadastroDeAnimais.sql
@@ -0,0 +1,13 @@
+CREATE DATABASE pet_care_db;
+USE pet_care_db;
+
+CREATE TABLE pets (
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  nome VARCHAR(50) NOT NULL,
+  especie VARCHAR(50) NOT NULL,
+  cor VARCHAR(50) NOT NULL,
+  idade INT,
+  ultimo_local_visto VARCHAR(100)
+);
+
+select*from pets;
